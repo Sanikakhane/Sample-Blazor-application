7fc15998bd0ef5f735b514ad32786ecefb32e778
Khane <Sanika.Khane@Klingelnberg.com>
2025-01-07 11:34:05 +0530

    Created Popup and working with Paramter and errors

--- BethanysPieShopHRM/Components/Pages/EmployeeOverview.razor.cs
+++ BethanysPieShopHRM/Components/Pages/EmployeeOverview.razor.cs
@@ -6,11 +6,16 @@ namespace BethanysPieShopHRM.Components.Pages
     public partial class EmployeeOverview
     {
         public List<Employee> Employees { get; set; } = default!;
-
+        private Employee? _selectEmployee;
         protected async override Task OnInitializedAsync()
         {
             await Task.Delay(2000);
             Employees = MockDataService.Employees;
+        }
+
+        public void ShowQuickViewPopup(Employee selectEmployee)
+        {
+            _selectEmployee = selectEmployee; 
         }
     }
 }
